2baf72ecc412675eb2d5a7eabfa0831a2e5c530e
Travis Baumgart <baumgartt@bit-sys.com>
2015-01-30 09:41:59 -0700

    Removed 0.0,0.0 Marker.

--- app/src/main/java/com/example/travis/bitsmap/MapsActivity.java
+++ app/src/main/java/com/example/travis/bitsmap/MapsActivity.java
@@ -5,8 +5,6 @@ import android.support.v4.app.FragmentActivity;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity {
 
@@ -60,6 +58,6 @@ public class MapsActivity extends FragmentActivity {
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        //mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
     }
 }
